1613c708c8766fd9ed9abf1268ead1f638a7ce0a
swperb <stephenproctor291@gmail.com>
2024-04-07 22:56:43 -0500

    Fixing E2E test bugs

--- bmi_calc_e2e.test.js
+++ bmi_calc_e2e.test.js
@@ -2,38 +2,35 @@ const { Builder, By } = require('selenium-webdriver');
 require('geckodriver'); // or another driver if you prefer a different browser
 
 describe('BMI Calculator E2E Tests', () => {
+    it('should calculate BMI correctly', async () => {
 
+        let driver = await new Builder().forBrowser('firefox').build();
 
-  let driver = new Builder().forBrowser('firefox').build();
+        // Test navigation to hosted application
+        await driver.get('https://bmi-calc-theta.vercel.app/');
 
+        // Test input of height-ft field
+        await driver.findElement(By.id('ft')).sendKeys('5');
 
-  afterAll(async () => {
-    await driver.quit();
-  });
+        // Test input of height-in field
+        await driver.findElement(By.id('in')).sendKeys('10');
 
-  it('should calculate BMI correctly', async () => {
+        // Test input of weight field
+        await driver.findElement(By.id('weight')).sendKeys('160');
 
-    // Test navigation to hosted application
-    await driver.get('https://bmi-calc-theta.vercel.app/');
+        // Test button click
+        await driver.findElement(By.css('button')).click();
 
-    // Test input of height-ft field
-    await driver.findElement(By.id('ft')).sendKeys('5');
+        // Test output of BMI, ensure BMI is calculated properly
+        const bmi = await driver.findElement(By.id('bmi')).getText();
+        expect(bmi).toBe('23.510');
 
-    // Test input of height-in field
-    await driver.findElement(By.id('in')).sendKeys('10');
+        // Test output of category, ensure category is determined properly
+        const category = await driver.findElement(By.id('category')).getText();
+        expect(category).toBe('Normal weight');
 
-    // Test input of weight field
-    await driver.findElement(By.id('weight')).sendKeys('160');
-
-    // Test button click
-    await driver.findElement(By.css('button')).click();
-
-    // Test output of BMI, ensure BMI is calculated properly
-    const bmi = await driver.findElement(By.id('bmi')).getText();
-    expect(bmi).toBe('23.510');
-
-    // Test output of category, ensure category is determined properly
-    const category = await driver.findElement(By.id('category')).getText();
-    expect(category).toBe('Normal weight');
-  });
+        
+        await driver.quit();
+        
+    });
 });
